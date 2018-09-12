07c47b0f02e3d54497a461dc8e63a0800e2e32af
Guilherme Bazilio <guilhermebazilio@gmail.com>
2018-09-12 00:20:23 -0300

    Separating methos to easy documentation of API

--- www/moments.js
+++ www/moments.js
@@ -1,22 +1,26 @@
-/*global cordova, module*/
+var moments = {};
 
-module.exports = {
-    initialize: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "initialize", []);
-    },
-    recordEvent: function (eventName, eventData, successCallback, errorCallback) {
-        if (eventData !== null && eventData !== undefined)
-            cordova.exec(successCallback, errorCallback, "MomentsPlugin", "recordEvent", [eventName, eventData]);
-        else
-            cordova.exec(successCallback, errorCallback, "MomentsPlugin", "recordEvent", [eventName]);
-    },
-    setFgTrackingMode: function (trackingMode, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "setFgTrackingMode", [trackingMode]);
-    },
-    setBgTrackingMode: function (trackingMode, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "setBgTrackingMode", [trackingMode]);
-    },
-    bestKnownLocation: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "bestKnownLocation", []);
-    },
+moments.initialize = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "MomentsPlugin", "initialize", []);
 };
+
+moments.recordEvent = function (eventName, eventData, successCallback, errorCallback) {
+    if (eventData !== null && eventData !== undefined)
+        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "recordEvent", [eventName, eventData]);
+    else
+        cordova.exec(successCallback, errorCallback, "MomentsPlugin", "recordEvent", [eventName]);
+};
+
+moments.setFgTrackingMode = function (trackingMode, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "MomentsPlugin", "setFgTrackingMode", [trackingMode]);
+};
+
+moments.setBgTrackingMode = function (trackingMode, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "MomentsPlugin", "setBgTrackingMode", [trackingMode]);
+};
+
+moments.bestKnownLocation = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "MomentsPlugin", "bestKnownLocation", []);
+};
+
+module.exports = moments;
