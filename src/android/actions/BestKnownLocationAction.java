5391c7ec219643153f818013a7e93e882e46ccd0
Guilherme Bazilio <guilhermebazilio@gmail.com>
2018-09-18 21:03:03 -0300

    Moving `wrap` method to JsonParser util

--- src/android/actions/BestKnownLocationAction.java
+++ src/android/actions/BestKnownLocationAction.java
@@ -6,15 +6,12 @@ import android.os.Bundle;
 import com.lotadata.moments.Moments;
 import com.lotadata.moments.plugin.actions.callback.Callback;
 import com.lotadata.moments.plugin.executors.Executor;
+import com.lotadata.moments.plugin.utils.JsonParser;
 
 import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Map;
 import java.util.Set;
 
 public class BestKnownLocationAction implements Action {
@@ -63,54 +60,11 @@ public class BestKnownLocationAction implements Action {
             Set<String> keys = extras.keySet();
             for (String key : keys) {
                 Object value = extras.get(key);
-                jsonExtras.put(key, wrap(value));
+                jsonExtras.put(key, JsonParser.wrap(value));
             }
             outputJson.put("extras", jsonExtras);
         }
 
         return outputJson;
-    }
-
-    private Object wrap(Object o) {
-        if (o == null) {
-            return "null";
-        }
-
-        if (o.getClass().isArray()) {
-            JSONArray innerArray = new JSONArray();
-            final int length = Array.getLength(o);
-            for (int i = 0; i < length; ++i) {
-                innerArray.put(wrap(Array.get(o, i)));
-            }
-            return innerArray;
-        }
-
-        if (o instanceof JSONArray || o instanceof JSONObject) {
-            return o;
-        }
-        try {
-            if (o instanceof Collection) {
-                return new JSONArray((Collection) o);
-            }
-            if (o instanceof Map) {
-                return new JSONObject((Map) o);
-            }
-            if (o instanceof Boolean ||
-                    o instanceof Byte ||
-                    o instanceof Character ||
-                    o instanceof Double ||
-                    o instanceof Float ||
-                    o instanceof Integer ||
-                    o instanceof Long ||
-                    o instanceof Short ||
-                    o instanceof String) {
-                return o;
-            }
-            if (o.getClass().getPackage().getName().startsWith("java.")) {
-                return o.toString();
-            }
-        } catch (Exception ignored) {
-        }
-        return "null";
     }
 }
--- src/android/utils/JsonParser.java
+++ src/android/utils/JsonParser.java
@@ -2,6 +2,11 @@ package com.lotadata.moments.plugin.utils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
 
 public class JsonParser {
     public static String getJsParameterAsString(JSONArray data, int index) {
@@ -20,5 +25,48 @@ public class JsonParser {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static Object wrap(Object o) {
+        if (o == null) {
+            return "null";
+        }
+
+        if (o.getClass().isArray()) {
+            JSONArray innerArray = new JSONArray();
+            final int length = Array.getLength(o);
+            for (int i = 0; i < length; ++i) {
+                innerArray.put(wrap(Array.get(o, i)));
+            }
+            return innerArray;
+        }
+
+        if (o instanceof JSONArray || o instanceof JSONObject) {
+            return o;
+        }
+        try {
+            if (o instanceof Collection) {
+                return new JSONArray((Collection) o);
+            }
+            if (o instanceof Map) {
+                return new JSONObject((Map) o);
+            }
+            if (o instanceof Boolean ||
+                    o instanceof Byte ||
+                    o instanceof Character ||
+                    o instanceof Double ||
+                    o instanceof Float ||
+                    o instanceof Integer ||
+                    o instanceof Long ||
+                    o instanceof Short ||
+                    o instanceof String) {
+                return o;
+            }
+            if (o.getClass().getPackage().getName().startsWith("java.")) {
+                return o.toString();
+            }
+        } catch (Exception ignored) {
+        }
+        return "null";
     }
 }
